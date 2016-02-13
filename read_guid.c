ba4ee6fedc79edf243f595820ad02a2de064a50f
Shujian Yang <yang_shujian@hotmail.com>
2016-02-12 23:38:46 -0600

    User can input file name after running.

--- read_guid.c
+++ read_guid.c
@@ -6,12 +6,21 @@ int main(int argc, char *argv[])
 {
     FILE * pfile;
     
-    if(argc == 1)
-        pfile = fopen("GPT.dd", "r");
-    else
+    if(argc == 1){
+        printf("Please enter file name: ");
+        char filename[31];
+        scanf("%30s", &filename);
+        pfile = fopen(filename, "r");
+    }
+    else if(argc == 2)
         pfile = fopen(argv[1], "r");
+    else{
+        printf("Error: Please provide 0 or 1 argument.\n");
+        exit(1);
+    }
     
     if(pfile != NULL){
+        int count = 0;
         while(!feof(pfile)){
             guid * g = (guid*)malloc(sizeof(guid));
             int err = read_guid_from_file(g, pfile);
@@ -23,7 +32,7 @@ int main(int argc, char *argv[])
                         
             char *s = (char*)malloc(LENGTH_OF_GUID);
             guid_encode(s, g);
-            printf("%s %s\n", s, get_guid_type(g));
+            printf("%03d. %s %s\n", ++count, s, get_guid_type(g));
             
             free(s);
             free(g);

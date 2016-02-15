545e5fae370515ecc4929dbe7dd6a2317fcf70bf
Shujian Yang <yang_shujian@hotmail.com>
2016-02-14 22:58:01 -0600

    Can process multiple files at a time.

--- read_guid.c
+++ read_guid.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "guid.h"
 
+void process_guid(FILE *pf);
+
+
 int main(int argc, char *argv[])
 {
     FILE * pfile;
@@ -11,40 +14,55 @@ int main(int argc, char *argv[])
         char filename[31];
         scanf("%30s", &filename);
         pfile = fopen(filename, "r");
-    }
-    else if(argc == 2)
-        pfile = fopen(argv[1], "r");
-    else{
-        printf("Error: Please provide 0 or 1 argument.\n");
-        exit(1);
-    }
-    
-    if(pfile != NULL){
-        int count = 0;
-        while(!feof(pfile)){
-            guid * g = (guid*)malloc(sizeof(guid));
-            int err = read_guid_from_file(g, pfile);
-            
-            if(err == -1){
-                printf("Extra bytes less than GUID length left.\n");
-                break;
-            }
-            else if(err){
-                break;
-            }
-                        
-            char *s = (char*)malloc(LENGTH_OF_GUID_STRING + 1);
-            guid_encode(s, g);
-            printf("%03d. %s %s\n", ++count, s, get_guid_type(g));
-            
-            free(s);
-            free(g);
+        if(pfile != NULL){
+            printf("%s:\n", filename);
+            process_guid(pfile);
+            fclose(pfile);
         }
-        
-        fclose(pfile);
+        else
+            printf("%s: File opening has failed.\n", filename);
+        printf("==============================================\n");
     }
-    else
-        printf("File opening has failed.\n");
+    else if(argc > 1){
+        int i;
+        for(i = 1; i < argc; ++i){
+            pfile = fopen(argv[i], "r");
+    
+            if(pfile != NULL){
+                printf("%s:\n", argv[i]);
+                process_guid(pfile);
+                fclose(pfile);
+            }
+            else
+                printf("%s: File opening has failed.\n", argv[i]);
+            printf("==============================================\n");
+        }
+    }
     
     return 0;
+}
+
+
+void process_guid(FILE *pfile)
+{
+    int count = 0;
+    while(!feof(pfile)){
+        guid * g = (guid*)malloc(sizeof(guid));
+        int err = read_guid_from_file(g, pfile);
+        
+        if(err == -1){
+            printf("Extra bytes less than GUID length left.\n");
+            break;
+        }
+        else if(err){
+            break;
+        }
+                    
+        char *s = (char*)malloc(LENGTH_OF_GUID_STRING + 1);
+        guid_encode(s, g);
+        printf("%03d. %s %s\n", ++count, s, get_guid_type(g));
+        
+        free(s);
+        free(g);
+    }
 }
